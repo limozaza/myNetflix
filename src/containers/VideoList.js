babda35082eeb30eba013e708a62f65b691a83ee
limozaza <zakboite@gmail.com>
2017-06-22 15:39:16 +0200

    map and key

--- src/containers/VideoList.js
+++ src/containers/VideoList.js
@@ -7,11 +7,12 @@ const VideoList = () => {
     const movies = ['film1','film2','film3','film4','film5']
     return (
         <ul>
-            <VideoListItem movie={movies[0]}/>
-            <VideoListItem movie={movies[1]}/>
-            <VideoListItem movie={movies[2]}/>
-            <VideoListItem movie={movies[3]}/>
-            <VideoListItem movie={movies[4]}/>
+            {
+                movies.map((movie,key) => {
+                    return <VideoListItem movie={movie} key={key}/>
+                })
+            }
+            
         </ul>
     );
 
